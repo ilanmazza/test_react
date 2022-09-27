90f73758757a489b45433dfa17c2ecd550d84cea
Wh1t3H4T <mc_ilan@hotmail.com>
2022-09-27 17:16:25 -0300

    modified:   public/index.html 	modified:   public/manifest.json 	deleted:    public/robots.txt 	modified:   src/index.js 	modified:   src/pages/HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,12 +4,18 @@ import Card from '../components/card/Card';
 import Grid from '../components/grid/Grid';
 import SimpleDialogDemo from '../components/dialog/SimpleDialogDemo';
 import { Container } from '@mui/system';
+import { Typography } from '@mui/material';
+
+import Box from '@mui/material/Box';
+
 
 function HomePage() {
   return (
     <div className="HomePage">
       <AppBar></AppBar>
       <SimpleDialogDemo></SimpleDialogDemo>
+      <Box sx={{ fontStyle: 'oblique' }}><Typography>API Cursos es una plataforma para realizar cursos directamente desde las ofertas creadas por los profesores. Es una aplicacion sin fines de lucro para compartir conocimiento.</Typography></Box>
+      <Box sx={{ fontStyle: 'normal' }}><Typography>Estos son los cursos mejor puntuados del mes</Typography></Box>      
       <Container>
           <Grid>
             <Card></Card>
