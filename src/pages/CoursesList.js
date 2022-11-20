b1fdefa7aeda87c9324702a4dcfa2a560bdf3745
matiasba <matiasabarros@hotmail.com>
2022-11-20 12:10:44 -0300

    Implementado el buscado de cursos

--- src/pages/CoursesList.js
+++ src/pages/CoursesList.js
@@ -2,38 +2,35 @@ import React, {useEffect,useState} from 'react';
 import AppBar from "../components/appbar/AppBar";
 import CustomCard from '../components/card/Card';
 import Grid2 from '@mui/material/Unstable_Grid2';
-import {GetTopCourses} from '../services/Courses'
+import {GetFilterCourses} from '../services/Courses'
 import TextField from '@mui/material/TextField';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import {Form, useNavigate} from 'react-router-dom';
-import Button from '@mui/material/Button';
+import {useNavigate, useSearchParams} from 'react-router-dom';
 
 
-export default function CoursesList() {
+export default function CoursesList () {
   const navigate = useNavigate()
+  const [searchParams] = useSearchParams();
+
   const [topCourses,setTopCourses] = useState([])
-  const [searchName,setSearchName] = useState('')
-  const [searchType,setSearchType] = useState('')
+  const [searchName,setSearchName] = useState(searchParams.get("search")|| '')
+  const [searchType,setSearchType] = useState(searchParams.get("type")|| '')
 
 
   useEffect(function() {
-    GetTopCourses().then(topCourses =>setTopCourses(topCourses))
-  },[setTopCourses])
+    GetFilterCourses(searchName,searchType).then(topCourses =>setTopCourses(topCourses))
+  },[searchName, searchType, setTopCourses, setSearchType, setSearchName])
 
   const handleChangeSearchName = event => {
     event.preventDefault()
     setSearchName(event.target.value)
+    navigate('/coursesList?search='+searchName+'&type='+searchType)
   }
 
   const handleChangeSearchType = event => {
     event.preventDefault()
     setSearchType(event.target.value)
-  }
-
-  const handleSearch = event => {
-    event.preventDefault()
     navigate('/coursesList?search='+searchName+'&type='+searchType)
   }
   
@@ -45,7 +42,6 @@ export default function CoursesList() {
       <Grid2 xs={12}>
       <Card sx={{ minWidth: 275 }}>
         <CardContent>
-        <Form onSubmit={handleSearch}>
           <TextField
             id="searchName"
             label="Curso"
@@ -58,13 +54,8 @@ export default function CoursesList() {
             value={searchType}
             onChange={handleChangeSearchType}
           />
-          </Form>
         </CardContent>
-        <CardActions style={{justifyContent: 'center'}}>
-          <Button color="success" variant="contained" onClick={handleSearch}>Buscar</Button>
-        </CardActions>
       </Card>
-
         </Grid2>
         {topCourses.map(lessons => (
           <CustomCard  key={lessons.name} raiting={lessons.raiting[0]} name={lessons.name} description={lessons.description} costo={lessons.costo} frecuencia={lessons.frecuencia} duracion={lessons.duracion} image='https://i.ytimg.com/vi/bYOjmW-740M/maxresdefault.jpg'/>
--- src/services/Courses.js
+++ src/services/Courses.js
@@ -1,14 +1,21 @@
-//import axios from 'axios'
-
 const baseUrl = 'http://localhost:3001/api/courses/filter'
-
-//export default function Login (credentials) {
-//  return axios.post(baseUrl, credentials)
-//}
 
 export function GetTopCourses () {
   return fetch(baseUrl+'?'+ new URLSearchParams({
     orderBy: 'raiting: -1',
+    limit: 5})).then(res => {
+    if (!res.ok) throw new Error ('api/cousers/filter endpoint response is NOT ok')
+    return res.json()
+  }).then(res => {
+    return res
+  })
+}
+
+export function GetFilterCourses (inName,inType) {
+  return fetch(baseUrl+'?'+ new URLSearchParams({
+    orderBy: 'raiting: -1',
+    name: inName,
+    type: inType,
     limit: 5})).then(res => {
     if (!res.ok) throw new Error ('api/cousers/filter endpoint response is NOT ok')
     return res.json()
